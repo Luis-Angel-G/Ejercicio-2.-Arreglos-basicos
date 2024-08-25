86c091b0b28a413c42ee327e3ad44323067d1553
Luis-Angel-G <langelgirona@gmail.com>
2024-08-24 19:56:08 -0600

    Se agrega el constructor, y geters y seters

---
+++ Desarrollador.java
@@ -0,0 +1,27 @@
+public class Desarrollador {
+	private String nombre;
+	private int puesto;
+	private String Contrasenia;
+	
+	public Desarrollador() {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(int puesto) {
+		this.puesto = puesto;
+	}
+	public String getContrasenia() {
+		return contrasenia;
+	}
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+}
