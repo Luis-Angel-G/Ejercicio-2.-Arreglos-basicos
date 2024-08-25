a3ac9ab7a4e904a15d5506b057ede2d63af1d180
Luis-Angel-G <langelgirona@gmail.com>
2024-08-24 20:05:26 -0600

    Se agrega el constructor, y geters y seters

---
+++ Tarea.java
@@ -0,0 +1,48 @@
+public class Tarea {
+	private String nombre;
+	private int tipo;
+	private double testimado;
+	private double treal;
+	private int estado;
+	private Desarrolador desarrollador;
+	
+	public Tarea() {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+	public double getTEstimado() {
+		return testimado;
+	}
+	public void setTEstimado(double testimado) {
+		this.testimado = testimado;
+	}
+	public double getTReal() {
+		return treal;
+	}
+	public void setTReal(double treal) {
+		this.treal = treal;
+	}
+	public int getEstado() {
+		return estado;
+	}
+	public void setEstado(int estado) {
+		this.estado = estado;
+	}
+	public Desarrollador getDesarrollador() {
+		return testimado;
+	}
+	public void setDesarrollador(Desarrollador desarrollador) {
+		this.desarrollador = desarrollador;
+	}
+}
