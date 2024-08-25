94afbaf34f690ed2111cd7458b46519a41df8206
Luis-Angel-G <langelgirona@gmail.com>
2024-08-24 20:59:44 -0600

    Se agregaron geters y seters y se modificó la linea 2 y 3 cambiando i por j, y en las lineas 4 y 5, j por k

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 public class Main {
-	private int i = 0;
-	Proyecto[] proyectos = new Proyecto[i];
 	private int j = 0;
-	Desarrollador[] desarrolladores = new Desarrollador[j];
+	Proyecto[] proyectos = new Proyecto[j];
+	private int k = 0;
+	Desarrollador[] desarrolladores = new Desarrollador[k];
 	
 	public Main() {
 		
+	}
+	public int getJ() {
+		return j;
+	}
+	public void setJ(int j) {
+		this.j = j;
+	}
+	public int getK() {
+		return k;
+	}
+	public void setK(int k) {
+		this.k = k;
 	}
 }
