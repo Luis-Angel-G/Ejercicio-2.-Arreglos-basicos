ce7eca2b17130aa5cbb085bbcda1a4d57487ca47
Luis-Angel-G <langelgirona@gmail.com>
2024-08-24 20:18:45 -0600

    Se agrega el constructor, y geters y seters

---
+++ Proyecto.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class Proyecto {
+	private String nombre;
+	private int categoria;
+	private Date fecha;
+	private int i = 0;
+	Tarea[] tareas = new Tarea[i];
+	private Desarrollador desarrolladorj;
+	private Desarrolador desarrolladors;
+	
+	public Proyecto() {
+		
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Desarrollador getDesarrolladorJ() {
+		return desarrolladorj;
+	}
+	public void setDesarrolladorJ(Desarrollador desarrolladorj) {
+		this.desarrolladorj = desarrolladorj;
+	}
+	public Desarrollador getDesarrolladorS() {
+		return desarrolladors;
+	}
+	public void setDesarrolladorS(Desarrollador desarrolladors) {
+		this.desarrolladors = desarrolladors;
+	}
+}
