4bf6e28c3bb4ca6b1c78d0f8d16edeffe1a2877a
Luis-Angel-G <langelgirona@gmail.com>
2024-08-24 20:23:02 -0600

    Se agrega el constructor

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	private int i = 0;
+	Proyecto[] proyectos = new Proyecto[i];
+	private int j = 0;
+	Desarrollador[] desarrolladores = new Desarrollador[j];
+	
+	public Main() {
+		
+	}
+}
